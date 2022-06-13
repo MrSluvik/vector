5f8b95cb3bde6c56b75d99b860f64409dbbb301e
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-06-13 23:21:55 +0300

    кинув ще раз

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -20,58 +20,58 @@
 #include<time.h>
 using namespace std;
 
-template<typename T>
-void print(vector<T>v) {//ф-н друку вектора
+
+void print(vector<int>v) {//ф-н друку вектора
 	for (int i = 0; i <v.size(); i++)
 	{
 		cout << v[i]<<"\t";
 	}
 }
 
-template<class T>
+
 class MinEl
 {
 public:
-	T operator ()(vector<T>v)
+	int operator ()(vector<int>v)
 	{
 		auto result = min_element(v.begin(), v.end());
 		return *result;
 	}	
 };
 
-template<class T>
+
 class MaxEl
 {
 public:	
-	int operator ()(vector<T>v)
+	int operator ()(vector<int>v)
 	{
 		auto result = max_element(v.begin(), v.end());
 		return *result;
 	}
 };
 
-template<class T>
+
 class Sort
 {
 public:
-	void operator()(vector<T>v) {
+	void operator()(vector<int>v) {
 		cout << "Сортування за зрозтанням : " << endl;
-		sort(v.begin(), v.end(), [](T a, T b) {return a < b;});
+		sort(v.begin(), v.end(), [](int a, int b) {return a < b;});
 		print(v); 
 		cout << endl;
 		cout << "\nСортування за спаданням : " << endl;
-		sort(v.begin(), v.end(), [](T a, T b) {return a > b; });
+		sort(v.begin(), v.end(), [](int a, int b) {return a > b; });
 		print(v); 
 		cout << endl;
 	}
 
 };
 
-template<class T>
+
 class Plus
 {
 public:	
-		void operator()(vector<T>v, T a) {
+		void operator()(vector<int>v, int a) {
 			cout << "\n+Вмістимість вектора після збільшення його розміру : " << endl;
 			v.resize(a);
 			cout << v.size() <<endl;
@@ -80,11 +80,11 @@ public:
 		}
 };
 
-template<class T>
+
 class Minus
 {
 public:
-	void operator()(vector<T>v,T a) {
+	void operator()(vector<int>v,int a) {
 		cout << "\n-Вмістимість вектора після зменшення  його розміру : " << endl;
 		v.resize(a);//дозволяє збільшити або зменшити кількість обєктів у векторі  reserve дозволяє лише збільшити кількість вмісту
 		cout << v.size() << endl;	//показує ємність вектора 	
@@ -93,11 +93,11 @@ public:
 	}
 };
 
-template<class T>
+
 class Delete {
 public:
-	void operator()(vector<T>v) {
-		T a;
+	void operator()(vector<int>v) {
+		int a;
 		cout<<"\nВведіть значення які хочете видалити"<<endl;
 		cin >>a;
 			cout << "\nВектор після видалення шуканого елементу:" << endl;
@@ -109,41 +109,39 @@ public:
 	}
 };
 
-
-
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(time(0));
-	vector<double>v= { 10.4, 10.1, 3.8, 9.3, 7.2, 3.6, 4.6 ,7.9};
-	
+	vector<int>v(7);	
 	int a = 0, b = 0;
-	/*for (int i = 0; i < v.size(); i++)
+
+	for (int i = 0; i < v.size(); i++)
 	{
 		v[i] = rand() % 90-20;
-	}*/
+	}
 	print(v);
 	cout << endl;
-	MinEl<double> minn;
+	MinEl minn;
 	cout<<"\nМінімальний елемент у векторі : " << minn(v) << endl<<endl;
-	MaxEl<double> maxx;
+	MaxEl maxx;
 	cout <<"Максимальний елемент у векторі : " << maxx(v) << endl << endl;
 
-	Sort<double> sortt;
+	Sort sortt;
 	sortt(v);
 	
-	Plus<double> Vec;
+	Plus Vec;
 	cout << "Введіть новий, більший розмір вектора : " << endl;
 	cin >> a;
 	Vec(v, a);
 
-	Minus<double> Ve;
+	Minus Ve;
 	cout << "Введіть новий, менший розмір вектора : " << endl;
 	cin >> b;
 	Ve(v, b);
 
-	Delete<double> d;
+	Delete d;
 	d(v);
 }
 
